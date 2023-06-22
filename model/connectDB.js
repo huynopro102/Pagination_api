922d13182e376c0e611941c732734d4ddb3425ad
tuan huy <100185063+huynopro102@users.noreply.github.com>
2023-06-22 23:07:14 +0700

    first commit

---
+++ model/connectDB.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dbURL = 'mongodb://127.0.0.1/Pagination_api'
+mongoose.connect(dbURL,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+
+});
+const Schema = mongoose.Schema;
+const item = new Schema({
+    username : String , 
+    url : String , 
+},{
+    collection: 'items'
+})
+
+const modelitem = mongoose.model('itemsModel',item)
+
+
+
+   
+
+
+
+module.exports = modelitem
+
